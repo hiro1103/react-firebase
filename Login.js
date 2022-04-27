0725922efaa71ebe5f617625b6a4ab464bf51ea7
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-28 02:40:33 +0900

    Add files via upload

--- Login.js
+++ Login.js
@@ -1,44 +1,27 @@
-import { auth } from "./firebase";
-import { Link, useHistory } from "react-router-dom";
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth,provider } from '../firebase';
+import { useHistory } from 'react-router-dom';
 import { useState } from 'react';
+import { signInWithPopup} from "firebase/auth";
 const Login = () => {
-    const history = useHistory();
-    const [error, setError] = useState('');
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const { email, password } = event.target.elements;
-        try {
-            await signInWithEmailAndPassword(auth, email.value, password.value);
-            history.push('/');
-        } catch (error) {
-            console.log(error);
-            setError(error.message);
-        }
-    };
-    return (
-        <div>
+  const history = useHistory();
+  const [error, setError] = useState('');
+  const handleLogin = async (event) => {
+    try {
+      await signInWithPopup(auth,provider);
+      history.push('/');
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
 
-            <h1 align="center" style={{ color: "red" }}><b>ユーザー登録画面</b></h1><br /><br />
-            <form onSubmit={handleSubmit}>
-                <table align="center">
-                    <tr align="center"><th>メールアドレス：</th>
-                        <td>
-                            <input type="email" name="email" placeholder="email" />
-                        </td>
-                    </tr>
-
-                    <tr align="center"><th>パスワード入力：</th>
-                        <td><input type="password" name="password" /></td></tr>
-                    <tr align="center"><td><button>登録送信ボタン</button></td>
-                        <div align="center">
-                            ユーザ登録は<Link to={'/signup'}>こちら</Link>から
-                        </div>
-                    </tr>
-                </table>
-            </form>
-        </div >
-    )
-}
+  return (
+    <div>
+      <h1>ログイン</h1>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <button onClick={handleLogin}>Googleログイン</button>
+    </div>
+  );
+};
 
 export default Login;
