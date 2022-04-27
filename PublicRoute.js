b14e975a280eaddcf7f0087ddad6b59a05f5770b
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-28 02:45:21 +0900

    Add files via upload

--- PublicRoute.js
+++ PublicRoute.js
@@ -1,16 +1,15 @@
 import { Route, Redirect } from 'react-router-dom';
-import { useAuthContext } from './AuthContext';
-
+import { useAuthContext } from '../context/AuthContext';
 const PublicRoute = ({ component: Component, ...rest }) => {
-    const { user } = useAuthContext();
-    return (
-        <Route
-            {...rest}
-            render={(routeProps) => {
-                return !user ? <Component {...routeProps} /> : <Redirect to="/" />;
-            }}
-        />
-    );
+  const { user } = useAuthContext();
+  return (
+    <Route
+      {...rest}
+      render={(routeProps) => {
+        return !user ? <Component {...routeProps} /> : <Redirect to="/" />;
+      }}
+    />
+  );
 };
 
 export default PublicRoute;
