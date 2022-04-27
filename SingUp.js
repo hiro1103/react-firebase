9ea54be8a2762a2e65c9cb6a60ff3beb7fb9d426
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-28 03:06:42 +0900

    Add files via upload

--- SingUp.js
+++ SingUp.js
@@ -1,5 +1,6 @@
+  
 import React from 'react';
-import { auth } from './firebase';
+import { auth } from '../firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 import { useRef } from 'react';
 import { useState } from 'react';
